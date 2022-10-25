81b2fba26c60568feef01ca90902bf5d755a6c22
Emmanoel Paim <emmanoel.paim@simers-rs.local>
2022-10-25 08:41:54 -0300

    Padronização nos template de certificado

--- modules/participante/controllers/CertificadoController.php
+++ modules/participante/controllers/CertificadoController.php
@@ -121,9 +121,9 @@ class CertificadoController extends Controller
         if ($_GET["troteid"] == 1) {
             $htmlContent = $this->renderPartial('certificado', ['model' => $certificado]);
         } else if ($_GET["troteid"] == 3) {
-            $htmlContent = $this->renderPartial('certificado20221', ['model' => $certificado]);
+            $htmlContent = $this->renderPartial('certificado202221', ['model' => $certificado]);
         } else {
-            $htmlContent = $this->renderPartial('certificado2022', ['model' => $certificado]);
+            $htmlContent = $this->renderPartial('certificado202211', ['model' => $certificado]);
         }
         $pdf->WriteHTML($htmlContent);
         $pdf->AddPage();
@@ -132,9 +132,9 @@ class CertificadoController extends Controller
         if ($_GET["troteid"] == 1) {
             $htmlContent = $this->renderPartial('certificado2', ['model' => $certificado]);
         } else if ($_GET["troteid"] == 3) {
-            $htmlContent = $this->renderPartial('certificado20222', ['model' => $certificado]);
+            $htmlContent = $this->renderPartial('certificado202222', ['model' => $certificado]);
         } else {
-            $htmlContent = $this->renderPartial('certificado22022', ['model' => $certificado]);
+            $htmlContent = $this->renderPartial('certificado202212', ['model' => $certificado]);
         }
         $pdf->WriteHTML($htmlContent);
         $pdf->defaultPagebreakType = '1';
