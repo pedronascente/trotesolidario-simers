396a88ff985f4991541f7adf8f6445289778197d
Emmanoel da Silva Soares Paim <emmanoel.paim@simers-rs.local>
2024-03-18 16:38:53 -0300

    Ajuste php versão 8.2

--- modules/participante/controllers/CertificadoController.php
+++ modules/participante/controllers/CertificadoController.php
@@ -125,6 +125,8 @@ class CertificadoController extends Controller
             $htmlContent = $this->renderPartial('certificado202221', ['model' => $certificado]);
         } else if ($_GET["troteid"] == 4 || $_GET["troteid"] == 5) {
             $htmlContent = $this->renderPartial('certificado202311', ['model' => $certificado]);
+        } else if ($_GET["troteid"] == 6 || $_GET["troteid"] == 7) {
+            $htmlContent = $this->renderPartial('certificado202411', ['model' => $certificado]);
         }
 
 
@@ -138,6 +140,8 @@ class CertificadoController extends Controller
             $htmlContent = $this->renderPartial('certificado202222', ['model' => $certificado]);
         } else if ($_GET["troteid"] == 4 || $_GET["troteid"] == 5) {
             $htmlContent = $this->renderPartial('certificado202312', ['model' => $certificado]);
+        } else if ($_GET["troteid"] == 6 || $_GET["troteid"] == 7) {
+            $htmlContent = $this->renderPartial('certificado202412', ['model' => $certificado]);
         } else {
             $htmlContent = $this->renderPartial('certificado202212', ['model' => $certificado]);
         }
