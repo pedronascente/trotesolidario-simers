d0844a741f705787c4d2b9c632ac446ce880cee9
Pedro jardim <pedro.jardim@simers.org.br>
2026-04-14 13:18:40 -0300

    modificado arquivo de configuração de banco

--- src/config/db.php
+++ src/config/db.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=trotesolidario-mysql-1;dbname=trotesolidario',
+    'dsn' => 'mysql:host=mysql-db;dbname=trotesolidario',
     'username' => 'trote',
     'password' => 'trote',
     'charset' => 'utf8',
