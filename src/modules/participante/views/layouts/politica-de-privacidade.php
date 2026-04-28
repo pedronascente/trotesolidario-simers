388511efd04494788b0ffa3c536c344508f9d432
Pedro jardim <pedro.jardim@simers.org.br>
2026-04-28 16:51:52 -0300

    politica-de-privacidade.php

---
+++ src/modules/participante/views/layouts/politica-de-privacidade.php
@@ -0,0 +1,47 @@
+<?php
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use app\assets\AdminAsset;
+use app\widgets\Alert;
+
+AdminAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <?= $this->head() ?>
+        <!--Header-->
+        <title>Trote Solidario</title>
+        <meta name="title" content="Trote Solidario">
+        <meta name="description" content="Trote Solidario">
+        <meta itemprop="image" content="">
+        <!--Fim Header-->
+        <!--Twiter-->
+        <meta property="twitter:description" content="Trote Solidario">
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="twitter:title" content="Trote Solidario">
+        <meta property="twitter:image" content="">
+        <!--Fim Twiter-->
+        <!--Facebook-->
+        <meta property="og:url" content="">
+        <meta property="og:type" content="article">
+        <meta property="og:title" content="Trote Solidario">
+        <meta property="og:description" content="Trote Solidario">
+        <link rel="shortcut icon" href="/img/favicon_trote.png" type="image/x-icon">
+        <!--<meta property="og:image" content="/imagens/favicon-32x32.jpg">-->
+        <!--Fim Facebook-->
+        <!--<link rel="shortcut icon" href="/imagens/favicon.png" type="image/x-icon">-->
+        <!-- End Google Analytics -->
+    </head>
+    <body class="bg-gradient-success">
+        <?php $this->beginBody() ?>
+        <?= Alert::widget() ?>
+        <?= $content ?>
+        <?php $this->endBody() ?>
+    </body>
+</html>
+<?php $this->endPage() ?>
